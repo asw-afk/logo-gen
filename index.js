4ef203c0d5d0388d24a20c9cabef6c005ed008f8
asw <annasilverwolff@gmail.com>
2024-06-17 09:58:59 -0400

    shape function not interacting as intended

--- index.js
+++ index.js
@@ -2,38 +2,44 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const { createSvg } = require("./lib/svgGenerator");
 
-const questions = [ 
-    {
-        type: "input",
-        message: "Enter text for logo. (Must not be more than 3 characters.)",
-        name: "text",
-        validate: (value) => {
-            if (value.length <= 3) {
-                return true; 
-            }else {
-                return "stahp";
-            }
-        },
-},
-    {
-        type: "list",
-        message: "What shape do you want?",
-        name: "shape",
-        choices: ["triangle", "circle", "square"],
+const questions = [
+  {
+    type: "input",
+    message: "Enter text for logo. (Must not be more than 3 characters.)",
+    name: "text",
+    validate: (value) => {
+      if (value.length <= 3) {
+        return true;
+      } else {
+        return "stahp";
+      }
     },
-    {
-        type: "input", 
-        message: "Select a color",
-        name: "colors",
-    },
+  },
+  {
+    type: "list",
+    message: "What shape do you want?",
+    name: "shape",
+    choices: ["triangle", "circle", "square"],
+  },
+  {
+    type: "input",
+    message: "Select a color",
+    name: "colors",
+  },
 ];
 
+function init() {
+  inquirer.prompt(questions).then((data, response) => {
+    if (data.shape === "triangle") {
+      data.shape =
+        "<polygon points=150,0 300,300 0,300 style=fill:${this.color} />";
+    } else {
+        return false
+    }
 
-function init () {
-inquirer.prompt(questions).then((response) => {
-    const makeSvg = createSvg(response)
-    writeToFile("logo.svg", makeSvg);
-})};
-
+      const makeSvg = createSvg(response);
+      writeFile("logo.svg", makeSvg);
+    }
+  )};
 
 init();
