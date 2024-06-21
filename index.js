767e948ca4cee13354cca732e9669c0c7347a89b
asw <annasilverwolff@gmail.com>
2024-06-21 06:18:27 -0400

    trying to debug render method
    Bug introduced: fs.writeFile is returning undefined

--- index.js
+++ index.js
@@ -2,35 +2,45 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const { createSvg } = require("./lib/svgGenerator");
 const { Triangle, Circle, Square } = require("./lib/shape");
+const { textInput} = require("./lib/text");
 
-const questions = [
-  {
-    type: "input",
-    message: "Enter text for logo. (Must not be more than 3 characters.)",
-    name: "text",
-    validate: (value) => {
-      if (value.length <= 3) {
-        return true;
-      } else {
-        return "stahp";
-      }
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter text for logo. (Must not be more than 3 characters.)",
+      name: "text",
+      //if they enter in more than three characters this will stop it
+      validate: (value) => {
+        if (value.length <= 3) {
+          return true;
+        } else {
+          return "Please enter only three letters.";
+        }
+      },
     },
-  },
-  {
-    type: "list",
-    message: "What shape do you want?",
-    name: "shape",
-    choices: ["triangle", "circle", "square"],
-  },
-  {
-    type: "input",
-    message: "Select a color",
-    name: "color",
-  },
-];
+    {
+      type: "input",
+      message: "What text color do you want?",
+      name: "textColor",
+    },
+    {
+      type: "list",
+      message: "What shape do you want?",
+      name: "shape",
+      choices: ["triangle", "circle", "square"],
+    },
+    {
+      type: "input",
+      message: "Select a shape color",
+      name: "color",
+    },
+  ]);
+};
 
-function init() {
-  inquirer.prompt(questions).then((answers) => {
+const init = (text) => {
+  questions().then((answers) => {
+    let text;
     let svgShape;
 
     if (answers.shape === "triangle") {
@@ -43,27 +53,23 @@ function init() {
       console.log("shape not recognized");
       return;
     }
-
-    // switch (answers.shape) {
-    //   case "triangle":
-    //     svgShape = new Triangle();
-    //     break;
-    //   case "circle":
-    //     svgShape = new Circle();
-    //     break;
-    //   case "square":
-    //     svgShape = new Square();
-    //     break;
-    //   default:
-    //     console.log("shape not recognized");
-    //     break;
-    // }
-
     svgShape.setColor(answers.color);
 
-    createSvg(svgShape.render());
+    if (answers.text !== 0) {
+      text = new textInput();
+    } else {
+      return;
+    }
+
+    text.setTextColor(answers.color);
+console.log("got to text color")
+
+
+    fs.writeFile("logo.svg", textInput.render);
+
+    // createSvg(svgShape.render());
     // fs.writeFile("logo.svg", makeSvg);
   });
-}
+};
 
 init();
