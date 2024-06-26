472a2f485ee24df86118b196172fb6297cc39c11
asw <annasilverwolff@gmail.com>
2024-06-25 22:35:24 -0400

    added comments

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ const init = () => {
    
       let svgShape;
 
+      //An else if statement that graps user input and returns the corresponding string based on their choice
       if (answers.shape === "triangle") {
         svgShape = new Triangle();
       } else if (answers.shape === "circle") {
@@ -53,13 +54,20 @@ const init = () => {
         console.log("shape not recognized");
         return;
       }
+      //After the shape string is returned this sets the color of it based on their color input
       svgShape.setColor(answers.color);
 
+      //This passes the text class into a variable we can use
       let whatever = new Whatever();
 
+
+      //This sets the entries onto our new Text to the corresponding user input.
+
+      //This will set our Logo's three letters and colors
       whatever.setText( answers.text, answers.textColor);
+      //This will set the correct shape string into our Text string by using svgShape as a parameter
       whatever.setShape(svgShape);
-      console.log("Am i getting here");
+      console.log("SVG generated");
       fs.writeFile("logo.svg", whatever.render(), (err) => err && console.error);
     })
     .catch((err) => {console.log(err)});
